b2192a9a2b9c96790800d9ea463bc243ac8def0e
James Bradbury <jekbradbury@google.com>
2019-08-16 17:07:08 -0700

    wip stax2

---
+++ jax/experimental/stax2.py
@@ -0,0 +1,335 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Stax is a small but flexible neural net specification library from scratch.
+
+For an example of its use, see examples/resnet50.py.
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import functools
+import itertools
+import operator as op
+
+import numpy as onp
+from six.moves import reduce
+
+from jax import lax
+from jax import random
+from jax.scipy.special import logsumexp, expit
+import jax.numpy as np
+
+
+# Following the convention used in Keras and tf.layers, we use CamelCase for the
+# names of layer constructors, like Conv and Relu, while using snake_case for
+# other functions, like lax.conv and relu.
+
+
+def relu(x): return np.maximum(x, 0.)
+def softplus(x): return np.logaddexp(x, 0.)
+sigmoid = expit
+def elu(x): return np.where(x > 0, x, np.exp(x) - 1)
+def leaky_relu(x): return np.where(x >= 0, x, 0.01 * x)
+
+def logsoftmax(x, axis=-1):
+  """Apply log softmax to an array of logits, log-normalizing along an axis."""
+  return x - logsumexp(x, axis, keepdims=True)
+
+def softmax(x, axis=-1):
+  """Apply softmax to an array of logits, exponentiating and normalizing along an axis."""
+  unnormalized = np.exp(x - x.max(axis, keepdims=True))
+  return unnormalized / unnormalized.sum(axis, keepdims=True)
+
+def fastvar(x, axis, keepdims):
+  """A fast but less numerically-stable variance calculation than np.var."""
+  return np.mean(x**2, axis, keepdims=keepdims) - np.mean(x, axis, keepdims=keepdims)**2
+
+import nn.initializers
+
+
+# Layers
+
+# Each layer constructor function returns an (init_fun, apply_fun) pair, where
+#   init_fun: takes an rng key and an example input and returns params
+#   apply_fun: takes params, inputs, and an rng key and applies the layer.
+
+
+def Dense(out_dim, W_init=glorot(), b_init=randn()):
+  """Layer constructor function for a dense (fully-connected) layer."""
+  def init_fun(rng, x):
+    k1, k2 = random.split(rng)
+    return W_init(k1, (x.shape[-1], out_dim)), b_init(k2, (out_dim,))
+  def apply_fun(params, x, **kwargs):
+    W, b = params
+    return np.dot(x, W) + b
+  return init_fun, apply_fun
+
+
+def GeneralConv(dimension_numbers, out_chan, filter_shape,
+                strides=None, padding='VALID', W_init=None, b_init=randn(1e-6)):
+  """Layer construction function for a general convolution layer."""
+  lhs_spec, rhs_spec, out_spec = dimension_numbers
+  one = (1,) * len(filter_shape)
+  strides = strides or one
+  W_init = W_init or glorot(rhs_spec.index('O'), rhs_spec.index('I'))
+  def init_fun(rng, x):
+    filter_shape_iter = iter(filter_shape)
+    kernel_shape = [out_chan if c == 'O' else
+                    x.shape[lhs_spec.index('C')] if c == 'I' else
+                    next(filter_shape_iter) for c in rhs_spec]
+    bias_shape = [out_chan if c == 'C' else 1 for c in out_spec]
+    bias_shape = tuple(itertools.dropwhile(lambda x: x == 1, bias_shape))
+    k1, k2 = random.split(rng)
+    return W_init(k1, kernel_shape), b_init(k2, bias_shape)
+  def apply_fun(params, x, **kwargs):
+    W, b = params
+    return lax.conv_general_dilated(x, W, strides, padding, one, one,
+                                    dimension_numbers) + b
+  return init_fun, apply_fun
+Conv = functools.partial(GeneralConv, ('NHWC', 'HWIO', 'NHWC'))
+
+
+def GeneralConvTranspose(dimension_numbers, out_chan, filter_shape,
+                         strides=None, padding='VALID', W_init=None,
+                         b_init=randn(1e-6)):
+  """Layer construction function for a general transposed-convolution layer."""
+  lhs_spec, rhs_spec, out_spec = dimension_numbers
+  one = (1,) * len(filter_shape)
+  strides = strides or one
+  W_init = W_init or glorot(rhs_spec.index('O'), rhs_spec.index('I'))
+  def init_fun(rng, x):
+    filter_shape_iter = iter(filter_shape)
+    kernel_shape = [out_chan if c == 'O' else
+                    x.shape[lhs_spec.index('C')] if c == 'I' else
+                    next(filter_shape_iter) for c in rhs_spec]
+    bias_shape = [out_chan if c == 'C' else 1 for c in out_spec]
+    bias_shape = tuple(itertools.dropwhile(lambda x: x == 1, bias_shape))
+    k1, k2 = random.split(rng)
+    return W_init(k1, kernel_shape), b_init(k2, bias_shape)
+  def apply_fun(params, x, **kwargs):
+    W, b = params
+    return lax.conv_transpose(x, W, strides, padding, dimension_numbers) + b
+  return init_fun, apply_fun
+Conv1DTranspose = functools.partial(GeneralConvTranspose, ('NHC', 'HIO', 'NHC'))
+ConvTranspose = functools.partial(GeneralConvTranspose,
+                                  ('NHWC', 'HWIO', 'NHWC'))
+
+
+def BatchNorm(axis=(0, 1, 2), epsilon=1e-5, center=True, scale=True,
+              beta_init=zeros, gamma_init=ones):
+  """Layer construction function for a batch normalization layer."""
+  _beta_init = lambda rng, shape: beta_init(rng, shape) if center else ()
+  _gamma_init = lambda rng, shape: gamma_init(rng, shape) if scale else ()
+  axis = (axis,) if np.isscalar(axis) else axis
+  def init_fun(rng, x):
+    shape = tuple(d for i, d in enumerate(x.shape) if i not in axis)
+    k1, k2 = random.split(rng)
+    return _beta_init(k1, shape), _gamma_init(k2, shape)
+  def apply_fun(params, x, **kwargs):
+    beta, gamma = params
+    # TODO(phawkins): np.expand_dims should accept an axis tuple.
+    # (https://github.com/numpy/numpy/issues/12290)
+    ed = tuple(None if i in axis else slice(None) for i in range(np.ndim(x)))
+    beta = beta[ed]
+    gamma = gamma[ed]
+    mean, var = np.mean(x, axis, keepdims=True), fastvar(x, axis, keepdims=True)
+    z = (x - mean) / np.sqrt(var + epsilon)
+    if center and scale: return gamma * z + beta
+    if center: return z + beta
+    if scale: return gamma * z
+    return z
+  return init_fun, apply_fun
+
+
+def elementwise(fun, **fun_kwargs):
+  """Layer that applies a scalar function elementwise on its inputs."""
+  init_fun = lambda rng, x: ()
+  apply_fun = lambda params, x, **kwargs: fun(x, **fun_kwargs)
+  return init_fun, apply_fun
+Tanh = elementwise(np.tanh)
+Relu = elementwise(relu)
+Exp = elementwise(np.exp)
+LogSoftmax = elementwise(logsoftmax, axis=-1)
+Softmax = elementwise(softmax, axis=-1)
+Softplus = elementwise(softplus)
+Sigmoid = elementwise(sigmoid)
+Elu = elementwise(elu)
+LeakyRelu = elementwise(leaky_relu)
+
+
+def _pooling_layer(reducer, init_val, rescaler=None):
+  def PoolingLayer(window_shape, strides=None, padding='VALID'):
+    """Layer construction function for a pooling layer."""
+    strides = strides or (1,) * len(window_shape)
+    rescale = rescaler(window_shape, strides, padding) if rescaler else None
+    dims = (1,) + window_shape + (1,)  # NHWC
+    strides = (1,) + strides + (1,)
+    init_fun = lambda rng, x: ()
+    def apply_fun(params, x, **kwargs):
+      out = lax.reduce_window(x, init_val, reducer, dims, strides, padding)
+      return rescale(out, x) if rescale else out
+    return init_fun, apply_fun
+  return PoolingLayer
+MaxPool = _pooling_layer(lax.max, -np.inf)
+SumPool = _pooling_layer(lax.add, 0.)
+
+
+def _normalize_by_window_size(dims, strides, padding):
+  def rescale(outputs, inputs):
+    one = np.ones(inputs.shape[1:-1], dtype=inputs.dtype)
+    window_sizes = lax.reduce_window(one, 0., lax.add, dims, strides, padding)
+    return outputs / window_sizes[..., np.newaxis]
+  return rescale
+AvgPool = _pooling_layer(lax.add, 0., _normalize_by_window_size)
+
+
+def Flatten():
+  """Layer construction function for flattening all but the leading dim."""
+  init_fun = lambda rng, x: ()
+  def apply_fun(params, x, **kwargs):
+    return np.reshape(x, (x.shape[0], -1))
+  return init_fun, apply_fun
+Flatten = Flatten()
+
+
+def Identity():
+  """Layer construction function for an identity layer."""
+  init_fun = lambda rng, x: ()
+  apply_fun = lambda params, x, **kwargs: x
+  return init_fun, apply_fun
+Identity = Identity()
+
+
+def FanOut(num):
+  """Layer construction function for a fan-out layer."""
+  init_fun = lambda rng, x: ()
+  apply_fun = lambda params, x, **kwargs: [x] * num
+  return init_fun, apply_fun
+
+
+def FanInSum():
+  """Layer construction function for a fan-in sum layer."""
+  init_fun = lambda rng, inputs: ()
+  apply_fun = lambda params, inputs, **kwargs: sum(inputs)
+  return init_fun, apply_fun
+FanInSum = FanInSum()
+
+
+def FanInConcat(axis=-1):
+  """Layer construction function for a fan-in concatenation layer."""
+  init_fun = lambda rng, inputs: ()
+  def apply_fun(params, inputs, **kwargs):
+    return np.concatenate(inputs, axis)
+  return init_fun, apply_fun
+
+
+def Dropout(rate, mode='train'):
+  """Layer construction function for a dropout layer with given rate."""
+  init_fun = lambda rng, x: ()
+  def apply_fun(params, x, **kwargs):
+    rng = kwargs.get('rng', None)
+    if rng is None:
+      msg = ("Dropout layer requires apply_fun to be called with a PRNG key "
+             "argument. That is, instead of `apply_fun(params, inputs)`, call "
+             "it like `apply_fun(params, inputs, key)` where `key` is a "
+             "jax.random.PRNGKey value.")
+      raise ValueError(msg)
+    if mode == 'train':
+      keep = random.bernoulli(rng, rate, inputs.shape)
+      return np.where(keep, inputs / rate, 0)
+    else:
+      return inputs
+  return init_fun, apply_fun
+
+
+# Composing layers via combinators
+
+
+def serial(*layers):
+  """Combinator for composing layers in serial.
+
+  Args:
+    *layers: a sequence of layers, each an (init_fun, apply_fun) pair.
+
+  Returns:
+    A new layer, meaning an (init_fun, apply_fun) pair, representing the serial
+    composition of the given sequence of layers.
+  """
+  nlayers = len(layers)
+  init_funs, apply_funs = zip(*layers)
+  def init_fun(rng, x):
+    params = []
+    for (init_fun, apply_fun) in zip(init_funs, apply_funs):
+      rng, layer_rng = random.split(rng)
+      param = init_fun(layer_rng, x)
+      x = apply_fun(param, x, rng=layer_rng)
+      params.append(param)
+    return params
+  def apply_fun(params, x, **kwargs):
+    rng = kwargs.pop('rng', None)
+    rngs = random.split(rng, nlayers) if rng is not None else (None,) * nlayers
+    for fun, param, rng in zip(apply_funs, params, rngs):
+      inputs = fun(param, inputs, rng=rng, **kwargs)
+    return inputs
+  return init_fun, apply_fun
+
+
+def parallel(*layers):
+  """Combinator for composing layers in parallel.
+
+  The layer resulting from this combinator is often used with the FanOut and
+  FanInSum layers.
+
+  Args:
+    *layers: a sequence of layers, each an (init_fun, apply_fun) pair.
+
+  Returns:
+    A new layer, meaning an (init_fun, apply_fun) pair, representing the
+    parallel composition of the given sequence of layers. In particular, the
+    returned layer takes a sequence of inputs and returns a sequence of outputs
+    with the same length as the argument `layers`.
+  """
+  nlayers = len(layers)
+  init_funs, apply_funs = zip(*layers)
+  def init_fun(rng, inputs):
+    rngs = random.split(rng, nlayers)
+    return [init(rng, x) for init, rng, x in zip(init_funs, rngs, inputs)]
+  def apply_fun(params, inputs, **kwargs):
+    rng = kwargs.pop('rng', None)
+    rngs = random.split(rng, nlayers) if rng is not None else (None,) * nlayers
+    return [f(p, x, rng=r, **kwargs) for f, p, x, r in
+            zip(apply_funs, params, inputs, rngs)]
+  return init_fun, apply_fun
+
+
+def shape_dependent(make_layer):
+  """Combinator to delay layer constructor pair until input shapes are known.
+
+  Args:
+    make_layer: a one-argument function that takes an example input as an
+      argument and returns an (init_fun, apply_fun) pair.
+
+  Returns:
+    A new layer, meaning an (init_fun, apply_fun) pair, representing the same
+    layer as returned by `make_layer` but with its construction delayed until
+    input shapes are known.
+  """
+  def init_fun(rng, inputs):
+    return make_layer(inputs)[0](rng, inputs)
+  def apply_fun(params, inputs, **kwargs):
+    return make_layer(inputs)[1](params, inputs, **kwargs)
+  return init_fun, apply_fun
